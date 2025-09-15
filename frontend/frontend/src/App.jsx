16cb5d2e15b078174823bdf0eb1a549895ba3f58
Carlos Azcarraga <tasajos@gmail.com>
2025-09-15 09:02:03 -0400

    init3

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,10 +1,9 @@
-/* src/App.jsx */
-
+// src/App.jsx
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import CategoryBar from './components/CategoryBar';
 import Footer from './components/Footer';
-import CategoryBar from './components/CategoryBar'; // Importa el nuevo componente
 
 function App() {
   const [productos, setProductos] = useState([]);
@@ -27,7 +26,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* Pasa las props del buscador al Navbar */}
       <Navbar searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
       <CategoryBar />
       <header className="main-header">
@@ -38,9 +36,11 @@ function App() {
         {filteredProducts.length > 0 ? (
           filteredProducts.map(producto => (
             <div key={producto.id} className="producto">
+              {/* This is the missing code! */}
+              <img src={producto.imagen} alt={producto.nombre} className="producto-imagen" />
               <h2>{producto.nombre}</h2>
               <p>{producto.descripcion}</p>
-              <p className="producto-precio">Precio: ${producto.precio}</p>
+              <p className="producto-precio">Precio: Bs {producto.precio}</p>
             </div>
           ))
         ) : (
